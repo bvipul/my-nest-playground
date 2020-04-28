279f86cfeda7b979c406fb8a8343c237b286a970
Vipul Basapati <basapativipulkumar@gmail.com>
2020-04-28 13:30:39 +0800

    removed ormconfig.json

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,7 +5,19 @@ import { AppService } from './app.service';
 import { CoursesModule } from './courses/courses.module';
 
 @Module({
-	imports: [TypeOrmModule.forRoot(), CoursesModule],
+	imports: [
+		TypeOrmModule.forRoot({
+			type: 'mysql',
+			host: process.env.DB_HOST || 'localhost',
+			port: 3306,
+			username: process.env.DB_USER || 'bvipul',
+			password: process.env.DB_PASS || 'root',
+			database: process.env.DB || 'mynestplayground',
+			entities: ['dist/**/*.entity.js'],
+			synchronize: true,
+		}),
+		CoursesModule,
+	],
 	controllers: [AppController],
 	providers: [AppService],
 })
